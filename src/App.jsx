69151732f5f27bf47243da9091dd66f1f5d9fe24
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-18 16:59:24 +0100

    add delete n modify onClick

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 
-// const books = [
+// const store = [
 //   {
 //     id: 1,
 //     title: "Il Signore degli Anelli",
@@ -44,19 +44,50 @@ import './App.css'
 const store = ["Moby Dick", "Il Grande Gatsby", "Orgoglio e Pregiudizio", "George Orwell", "Il Signore degli Anelli"]
 function App() {
   const [books, setBooks] = useState(store)
+  const [newBook, setNewBook] = useState('')
 
-  console.log(books);
+  function addBook(e) {
+    e.preventDefault()
+    const newBooks = [newBook, ...books]
+    setBooks(newBooks)
+    setNewBook('')
+  }
+  function deleteBook(e) {
+    const dataIndex = e.target.getAttribute('data-index')
+    const updatebooks = books.filter((book, index) => dataIndex != index)
+    setBooks(updatebooks)
+  }
+  function modifyBook(e) {
+    const dataIndex = e.target.getAttribute('data-index')
+    const updatebooks = books.filter((book, index) => dataIndex != index)
+    const modifyBook = prompt('change name')
+    const modifyList = [modifyBook, ...updatebooks]
+    setBooks(modifyList)
+  }
 
   return (
     <>
-      <header>
-        <h1>Leggere fa bene</h1>
-      </header>
-      <main>
-        {books.map((book, index) => {
-          <span>{book}</span>
-        })}
-      </main>
+      <h1>titolo</h1>
+      <form onSubmit={addBook}>
+        <div className="row">
+          <label htmlFor="book">Book</label>
+        </div>
+        <div className="inputs">
+          <input type="text" value={newBook} onChange={e => setNewBook(e.target.value)} />
+          <button type='submit'>ADD</button>
+        </div>
+
+      </form>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>
+            <span>{book}</span>
+            <button data-index={index} onClick={modifyBook}>Modify</button>
+            <button data-index={index} onClick={deleteBook}>Delete</button>
+
+          </li>
+        ))}
+      </ul>
 
 
     </>
